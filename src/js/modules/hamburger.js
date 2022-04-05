a98637502d6449ba7158bd1fdc16e679fbc4b0ae
Oleg <olegjan9@rambler.ru>
2022-04-05 11:18:14 +0300

    03/04/2022

--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -1,8 +1,10 @@
 const hamburger = () => {
-    const hamburger = document.querySelector(".hamburger-box"),
+    const hamburger = document.querySelector(".hamburger"),
         menu = document.querySelector(".menu"),
         close = document.querySelector(".close"),
-        menuBackground = document.querySelector(".menu__background");
+        menuBackground = document.querySelector(".menu__background"),
+        headerContent = document.querySelector(".menu .header__content-ul"),
+        headerMain = document.querySelector(".header__main");
 
     function hideMenu() {
         hamburger.classList.remove("is-active");
@@ -10,17 +12,33 @@ const hamburger = () => {
         hamburger.parentElement.classList.remove("is-active");
         menuBackground.style.visibility = "hidden";
         menuBackground.style.opacity = "0";
+        // hideHeaderContent();
+        // headerMain.prepend(headerContent);
+        // headerContent.style.display = "none";
+    }
+
+    headerContent.style.display = "block";
+    function hideHeaderContent(){
+        headerContent.remove();       
+    }
+
+    function showHeaderContent(){
+        // menu.prepend(headerContent);
+        // headerContent.style.display = "block";
     }
 
     function showMenu() {
-        hamburger.parentElement.classList.add("is-active");
+        hamburger.classList.add("is-active");
         menu.style.left = "0px";
         menuBackground.style.visibility = "visible";
         menuBackground.style.opacity = "1";
+        // hideHeaderContent();
+        // showHeaderContent();  
     }
 
     hamburger.addEventListener("click", (e) => {
-        showMenu();        
+        showMenu();
+              
     });
 
     menuBackground.addEventListener("click", (e) => {
