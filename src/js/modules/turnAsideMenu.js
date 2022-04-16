775bc47d6364f5a22287350f80ae86ca8124b0cc
Oleg <olegjan9@rambler.ru>
2022-04-16 15:24:26 +0300

    14/04/2022

--- src/js/modules/turnAsideMenu.js
+++ src/js/modules/turnAsideMenu.js
@@ -1,8 +1,7 @@
 const turnAsideMenu = () => {
 
     const hamburger = document.querySelector("#aside"),
-        menu = document.querySelector(".aside__menu"),
-        menuWidth = getComputedStyle(menu).width;
+        menu = document.querySelector(".aside__menu");
     let i = 90;
 
     function activeHamburger() {
@@ -15,73 +14,49 @@ const turnAsideMenu = () => {
         hamburger.childNodes[0].classList.remove("hamburger-box_background");
     }
 
-    function showMenu() {
-        activeHamburger();
-
-        function animate({
-            timing,
-            draw,
-            duration
-        }) {
-
-            let start = performance.now();
-
-            requestAnimationFrame(function animate(time) {
-                // timeFraction изменяется от 0 до 1
-                let timeFraction = (time - start) / duration;
-                if (timeFraction > 1) timeFraction = 1;
-
-                // вычисление текущего состояния анимации
-                let progress = timing(timeFraction);
-
-                draw(progress); // отрисовать её
-
-                if (timeFraction < 1) {
-                    requestAnimationFrame(animate);
-                }
-
-            });
+    function plusHeightMenu() {
+        i += 40;
+        if (i <= 703) {
+            menu.style.maxHeight = `${i}px`;
+            requestAnimationFrame(plusHeightMenu);
+        } else {
+            menu.style.maxHeight = `703px`;
         }
+    }
 
-        animate({
-            duration: 500,
-            timing(timeFraction) {
-                return timeFraction;
-            },
-            draw(progress) {
-                menu.style.maxHeight = progress * 701 + 'px'               
-            }
-        });
+    function minusHeightMenu() {
+        i -= 40;
+        if (i >= 90) {
+            menu.style.maxHeight = `${i}px`;
+            requestAnimationFrame(minusHeightMenu);
+        } else {
+            menu.style.maxHeight = `90px`;
+        }
+    }
+
+    function showMenu() {
+        menu.style.overflow = "visible";
+        activeHamburger();
+        plusHeightMenu();
     }
 
     function hideMenu() {
         deactivationHamburger();
-        // menu.classList.remove("aside__menu-activation");        
-        function myAnimation() {
-            i -= 40;
-            menu.style.maxHeight = `${i}px`;
-            console.log(i);
-            if (i > 90) {
-                requestAnimationFrame(myAnimation);
-
-            }
-        }
-        myAnimation();
-
+        minusHeightMenu();
+        menu.style.overflow = "hidden";
     }
+
     window.addEventListener("click", (e) => {
         if ((e.target && e.target.closest('#aside')) &&
-            menu.matches('.aside__menu-activation')) {
+            e.target.closest('.is-active')) {
             hideMenu();
         } else if ((e.target && e.target.closest('#aside')) &&
-            !menu.matches('.aside__menu-activation')) {
+            !e.target.closest('.is-active')) {
             showMenu();
         }
 
         if (!e.target.closest('#aside') &&
-            !e.target.closest('.aside-menu') &&
-            !e.target.matches('.aside__menu-link') &&
-            !e.target.parentNode.matches('.aside__menu-link')) {
+            !e.target.closest('.aside__menu-links')) {
             hideMenu();
         }
     });
