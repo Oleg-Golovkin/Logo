57473b04582f94d8caa09631368c25c6fc8c7540
Oleg <olegjan9@rambler.ru>
2022-04-17 18:19:18 +0300

    17/04/2022

--- src/js/modules/showAsideMenu.js
+++ src/js/modules/showAsideMenu.js
@@ -7,71 +7,37 @@ const showAsideMenu = () => {
         // Получаю ширину одного из них. По умолчанию в css ширина их 0px     
         asideMenuLink = document.querySelectorAll('.aside__menu-link_main'),
         asideMenuLinks = document.querySelector('.aside__menu-links');
-    let classActive;
     let maxWidth1200px;
-    const screenWidth = window.innerWidth;
+    const MediaQueryList = window.matchMedia("(max-width: 1200px)");
 
-    if (screenWidth <= 1200) {
-        classActive = "animate__fadeInDown";
-        maxWidth1200px = true;
-    } else {
-        classActive = "aside__menu-drawing_active";
-        maxWidth1200px = false;
+
+    function removeClassList(classList, ...rest) {
+        asideMenuDrawings.forEach(item => {
+            item.classList.remove(classList, ...rest);
+        });
     }
 
-    const mQuery = window.matchMedia('(max-width: 1200px)');
-
-    function handleMobilePhoneResize(e) {   
-       // Проверяем, верен ли медиа-запрос
-       if (e.target.matches) {     
-            // Затем выводим в консоль следующее сообщение
-            console.log('Media Query Matched!') ;  
-       } 
-    } 
-    
-    // Настраиваем слушателя событий
-    mQuery.addEventListener("change", (e) => handleMobilePhoneResize(e));
-
-    // mQuery.onchange = (e) => {
-    //     if (e.matches) {
-    //     /* the viewport is 600 pixels wide or less */
-    //     console.log('This is a narrow screen — less than 600px wide.')
-    //   } else {
-    //     /* the viewport is more than 600 pixels wide */
-    //     console.log('This is a wide screen — more than 600px wide.')
-    //   }
-    // }
-
-    function mediaWidth(e) {
-        e.stopPropagation();
-        if (e.target.matchMedia("(max-width: 1200px)").matches) {
-            classActive = "animate__fadeInDown";
+    function handleMobilePhoneResize(e) {
+        if (e.matches) {
             maxWidth1200px = true;
-        } else if (e.target.matchMedia("(min-width: 1201px)").matches) {
-            e.target.removeEventListener("resize", mediaWidth);
-            classActive = "aside__menu-drawing_active";
+            // Стираю все классы для монитора свыше 1200px
+            removeClassList("aside__menu-drawing_active");
+        } else {
             maxWidth1200px = false;
+            // стираю все классы, для понитора до 1200px
+            removeClassList("animate__fadeInDown", "hide", "showBlock");
         }
-
     }
-
-    window.addEventListener("resize", (e) => mediaWidth(e));
-
-    // Функции выезда меню:
-    // 1. При нажатии на активированную ссылку (меню выехало) - выехавшее 
-    // меню сворачивается).
-    // 2. При нажитии на не активную ссылку, но которая может задействовать 
-    // выезд другого меню, то активное меню (п.1) сворачивается, и выезжает новое 
-    // меню.
-    // 3. При нажии на любую другу ссылку (не активирующее меню) выехавшее меню сворачивается
+    // Настраиваем слушателя событий
+    MediaQueryList.addEventListener("change", (e) => handleMobilePhoneResize(e));
+    handleMobilePhoneResize(MediaQueryList);
 
     function disactiveClassView() {
         asideMenuLink.forEach(item => {
             item.classList.remove("aside__menu-link_main-view");
         });
     }
-
-
+    
     // 1. Подсвечивание фона на нажимаемых элементах меню и
     // убирание фона при повторном нажатии на сам элемент, или последующем
     // нажитии на другой элемент меню
@@ -93,19 +59,24 @@ const showAsideMenu = () => {
     // Событие на window, чтобы иметь возможность закрывать ссылки при 
     // нажатии на свободное пространство
     window.addEventListener("click", (e) => {
-        e.stopPropagation();
-        asideMenuDrawings.forEach((item, i) => {
-            item.classList.remove(classActive);
+        asideMenuDrawings.forEach((item, i) => {            
             if (maxWidth1200px) {
-                item.style.display = "none";
+                item.classList.remove("animate__fadeInDown");
+                item.classList.add("hide");
+            } else {
+                 // Действия для размера монитора выше 1200px
+                item.classList.remove("aside__menu-drawing_active");
             }
             if ((e.target.getAttribute('data-arrow') ||
                     e.target.parentNode.getAttribute('data-arrow')) == i) {
-                item.classList.toggle(classActive);
                 if (maxWidth1200px) {
-                    item.style.display = "block";
+                    item.classList.toggle("animate__fadeInDown");
+                    item.classList.add("showBlock");
+                    item.classList.remove("hide");
+                } else {
+                    // Действия для размера монитора выше 1200px
+                    item.classList.toggle("aside__menu-drawing_active");
                 }
-
             }
             // при нажатии на свободное пространство все закрывается
             if (!e.target.matches('.aside__menu-link') &&
