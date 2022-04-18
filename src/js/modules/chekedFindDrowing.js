5c146623e7138fdc0c091b0dbf666f45d0415870
Oleg <olegjan9@rambler.ru>
2022-04-18 12:09:50 +0300

    17/04/2022

--- src/js/modules/chekedFindDrowing.js
+++ src/js/modules/chekedFindDrowing.js
@@ -1,5 +1,15 @@
 const chekedFindDrowing = () => {
-    
+    const contentFindLinks = document.querySelectorAll(".content__find-link"),
+        everywhere = document.querySelector(".content__find-btn-everywhere span");
+
+
+    contentFindLinks.forEach((contentFindLink, i) => {
+        contentFindLink.addEventListener("click", (e) => {
+            contentFindLink.classList.toggle("content__find-link_active");
+            let contentFindLinkActive = document.querySelectorAll(".content__find-link_active");
+            everywhere.textContent = `Везде ${contentFindLinkActive.length}`;
+        });
+    });
 };
 
 
