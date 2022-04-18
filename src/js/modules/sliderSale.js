85b6737264550b451bc4b1cb18ba6058bb6c40b2
Oleg <olegjan9@rambler.ru>
2022-04-18 17:24:35 +0300

    18.04.2022

--- src/js/modules/sliderSale.js
+++ src/js/modules/sliderSale.js
@@ -1,8 +1,13 @@
 const sliderSale = () => {
-
+    const slide = document.querySelectorAll(".offer__slide"),
+        slider = document.querySelector(".offer__slider"),
+        nextSlide = document.querySelector(".offer__slider-next"),
+        prevSlide = document.querySelector(".offer__slider-prev"),
+        sliderWrapper2 = document.querySelector(".offer__slider-wrapper-2"),
+        total = document.querySelector("#total"),
+        current = document.querySelector("#current"),
+        width = window.getComputedStyle(slider).width;
 };
 
 
 export default sliderSale;
-
-
