53d5b9d47b0333df774fcd110edfb8b6a5fc9108
Oleg <olegjan9@rambler.ru>
2022-04-23 19:51:43 +0300

    23/04/2022

--- src/js/modules/showFindDrawing.js
+++ src/js/modules/showFindDrawing.js
@@ -1,11 +1,12 @@
 const showFindDrawing = () => {
-    const btn = document.querySelector('.content__find-btn-everywhere'),
+    const btn = document.querySelectorAl('.content__find-btn-everywhere'),
         // Меню равной ширины, поэтому достаточно получить ширину одного из них
         asideMenuDrawing = document.querySelector('.content__find-drawing');
     // Получаю ширину одного из них. По умолчанию в css ширина их 0px
 
-    btn.addEventListener("click", function(e) {
-        asideMenuDrawing.classList.toggle('content__find-drawing-active');       
+    btn.addEventListener("click", function (e) {
+        asideMenuDrawing.classList.toggle('content__find-drawing-active');      
+
     });
 
 };
