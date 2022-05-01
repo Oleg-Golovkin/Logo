e5ddd89705d1fd422d015b6ea2723eff795bd66b
Oleg <olegjan9@rambler.ru>
2022-05-01 14:30:41 +0300

    01/05/2022

--- src/js/script.js
+++ src/js/script.js
@@ -8,37 +8,44 @@ import sliderSale from "./modules/sliderSale";
 import showSubMenuProducts from "./modules/showSubMenuProducts";
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
     /* После полной загрузки HTML выполняется JS */
     showHeaderMenu();
     showAsideMenu();
-    turnAsideMenu(); 
+    turnAsideMenu();
     sliderСompanies();
-    chekedFindDrowing();    
+    chekedFindDrowing();
     sliderSale({
         slideSelector: "#slider-content .slider-content__slide",
         sliderParentSelector: ".slider-content#slider-content",
-        sliderSelector:"#slider-content .slider-content__wrapper" ,
+        sliderSelector: "#slider-content .slider-content__wrapper",
         subWrapperSelector: "#slider-content .slider-content__subWrapper",
         tabs: true,
         counter: false,
-        btn: false,             
+        btn: false,
     });
-   
+
     sliderSale({
         slideSelector: "#slider-content_products .slider-content__slide_products",
         sliderParentSelector: ".slider-content_products#slider-content_products",
-        sliderSelector:"#slider-content_products .slider-content__wrapper_products" ,
-        subWrapperSelector: "#slider-content_products .slider-content__subWrapper_products",        
+        sliderSelector: "#slider-content_products .slider-content__wrapper_products",
+        subWrapperSelector: "#slider-content_products .slider-content__subWrapper_products",
         totalSelector: "#slider-content_products .slider-content__total_products",
         currentSelector: "#slider-content_products .slider-content__current_products",
-        nextSlideSelector: "#slider-content_products .slider-content__next-img_products",        
-        prevSlideSelector: "#slider-content_products .slider-content__prev_products",        
+        nextSlideSelector: "#slider-content_products .slider-content__next-img_products",
+        prevSlideSelector: "#slider-content_products .slider-content__prev_products",
         tabs: false,
         counter: true,
-        btn: true, 
+        btn: true,
     });
     showSubMenuProducts();
-    
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        loop: false,
+
+      
+       
+    });
+
 });
