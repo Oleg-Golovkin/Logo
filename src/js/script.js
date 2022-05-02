9b448028fb962debc4d2b793140894a107150903
Oleg <olegjan9@rambler.ru>
2022-05-02 19:47:27 +0300

    01/05/2022

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@ import sliderСompanies from "./modules/sliderСompanies";
 import chekedFindDrowing from "./modules/chekedFindDrowing";
 import sliderSale from "./modules/sliderSale";
 import showSubMenuProducts from "./modules/showSubMenuProducts";
-import Swiper from "./modules/swiper-bundle.min";
+import Swiper, { Navigation, Pagination } from "./modules/swiper-bundle.min";
 
 document.addEventListener("DOMContentLoaded", () => {
     /* После полной загрузки HTML выполняется JS */
@@ -25,34 +25,113 @@ document.addEventListener("DOMContentLoaded", () => {
         btn: false,
     });
 
-    sliderSale({
-        slideSelector: "#slider-content_products .slider-content__slide_products",
-        sliderParentSelector: ".slider-content_products#slider-content_products",
-        sliderSelector: "#slider-content_products .slider-content__wrapper_products",
-        subWrapperSelector: "#slider-content_products .slider-content__subWrapper_products",
-        totalSelector: "#slider-content_products .slider-content__total_products",
-        currentSelector: "#slider-content_products .slider-content__current_products",
-        nextSlideSelector: "#slider-content_products .slider-content__next-img_products",
-        prevSlideSelector: "#slider-content_products .slider-content__prev_products",
-        tabs: false,
-        counter: true,
-        btn: true,
-    });
+    // sliderSale({
+    //     slideSelector: "#slider-content_products .slider-content__slide_products",
+    //     sliderParentSelector: ".slider-content_products#slider-content_products",
+    //     sliderSelector: "#slider-content_products .slider-content__wrapper_products",
+    //     subWrapperSelector: "#slider-content_products .slider-content__subWrapper_products",
+    //     totalSelector: "#slider-content_products .slider-content__total_products",
+    //     currentSelector: "#slider-content_products .slider-content__current_products",
+    //     nextSlideSelector: "#slider-content_products .slider-content__next-img_products",
+    //     prevSlideSelector: "#slider-content_products .slider-content__prev_products",
+    //     tabs: false,
+    //     counter: true,
+    //     btn: true,
+    // });
     showSubMenuProducts();
 
     const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        // loop: true,
+//**** Колличество одновременного показа слайдов
         slidesPerView: 3,
+//**** Несколько строк слайдов. Установить 
+//**** размер обертки и каждого элемента слайда
         grid: {
-            rows: 3,
-            fill: "column"
+            rows: 2,
+            // fill: "column"
         },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+//**** Включение кнопок
+        //**** В свойства prevEl и nextEl вставляем 
+        //**** свои селекторы кнопок, созданные в любом месте
+        navigation: {           
+            prevEl: '.slider-content__prev_products',
+            nextEl: '.slider-content__next_products',
+            
+        },       
+//**** Пагинация. Точки, номер слайда и другое
+        pagination: {            
+            //**** Тип пагинации: 	'bullets' | 'fraction' | 'progressbar' | 'custom'
+            //**** fraction - количество слайдов цифрами
+            type: 'fraction',
+            //**** Своя настрока Описания текущего слайда и количества слайдов
+            // renderFraction: function(currentClass, totalClass) {
+            // return `Номер текущего слайда <span class="${currentClass}"></span>
+            //     /
+            //     <span class="${totalClass}"></span> Общее колличество слайдов`
+            // }
+            //**** Своя настрока Описания текущего слайда
+            // formatFractionCurrent: function(number) {}
+            //**** Своя настрока Описания общего количества слайдов
+            // formatFractionTotal: function(number) {}
+            //**** Перемещение пагинации в селектор "." или HTML элемент ""
+            el: ".slider-content__сounter_products-wpapper",
           },
+        spaceBetween: 31,
+//****  Автоматически подстраивается под размеры окна
+        watchSlidesProgress: true,
+//****  Скорость перехода между слайдами
+        speed: 100,
+//****  Бесконечная прокрутка, через видимый возврат
+        rewind: true,
+//****  Бесконечная прокрутка, через невидимый возврат.
+//****Не использовать с rewind
+        loop: false,
+//**** Предотваращает нажатие на ссылки на слайдах во время
+//****прокрутки
+        preventClicks: true,
+//**** С какого слайда начать показ слайдов. Начин. с нуля
+        initialSlide: 0,
+//****Эффект перехода между слайдами. Виды 
+//**** 'slide' | 'fade' | 'cube' | 'coverflow' | 'flip' | 'creative' | 'cards'
+//**** Не работает с grid
+        // effect: 'flip'
+//***/ Направление листания 'horizontal' | 'vertical'
+        direction: "horizontal",
+//**** Если включено, то создается доп. оболочка для каждого
+//**** слайда    
+        createElements: false,
+//**** Различные виды центрирования слайдов
+        centerInsufficientSlides: false,
+        centeredSlides: false,
+        centeredSlidesBounds: false,
+//**** Медиаправила. Устанавливают доп. параметры в зависимости от 
+//**** размера окна window или контейнера container
+//**** Работает в основном размерами и отступами
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+            // slidesPerView: 2,
+            // spaceBetween: 20
+            },
+            // when window width is >= 480px
+            480: {
+            // slidesPerView: 3,
+            // spaceBetween: 30
+            },
+            // when window width is >= 640px
+            640: {
+            // slidesPerView: 4,
+            // spaceBetween: 40
+            }
+        },
+//**** Адаптация слайдов в зависимости от размера окна
+        // autoHeight: true
+//**** Отключение сенсорного переключения слайдов
+        allowTouchMove: true,
+//**** Отключение листания назад слайдов
+        allowSlidePrev: true,
+//**** Отключение листания вперед слайдов
+        allowSlideNext: true,
+        
+        
     });
-
-
 });
